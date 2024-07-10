f5153107b24ad5451fa8e2f4d0f9b0b40b32e656
Halil Özcan <131258476+Halil-x27@users.noreply.github.com>
2024-07-10 13:23:43 +0300

    Add files via upload

---
+++ Generic_Array_List/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections;
+
+namespace MyNamespace
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            ArrayList liste = new ArrayList();
+             liste.Add("Halil");
+             liste.Add(10);
+             liste.Add(true);
+             liste.Add('A');
+
+            // İçerisindeki verilere erişme
+
+             Console.WriteLine(liste[1]);
+
+            foreach (var item in liste)
+            {
+                Console.WriteLine(item);
+            }
+
+            // Addrange
+
+            string [] renkler = {"kırmızı", "mavi","yesil"};
+            List<int> sayilar = new List<int>(){1,3,5,7,9,12};
+             liste.AddRange(renkler);
+            liste.AddRange(sayilar);
+
+            foreach (var item in liste)
+            {
+                Console.WriteLine(item);
+            }
+
+            //sort 
+            liste.Sort();
+
+            
+            foreach (var item in liste)
+            {
+                Console.WriteLine(item);
+            }
+
+            //BinarySearch
+
+            Console.WriteLine(liste.BinarySearch(9));//düzgün çalışması için sıralama yapmamız gerekiyor.
+
+            // Reverse
+
+            liste.Reverse();
+            foreach (var item in liste)
+            {
+                Console.WriteLine(item);
+            }
+
+            //Clear
+
+            liste.Clear();
+            foreach (var item in liste)
+            {
+                Console.WriteLine(item);
+            }
+
+
+
+
+        }
+    }
+}
